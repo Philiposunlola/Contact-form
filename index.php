99a06cf35f7aaed6ab9522d4ffbec25b4a73030e
Philip <philiposun@gmail.com>
2023-09-25 02:46:28 +0100

    contact form

---
+++ index.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewpoint" content="width=device-width, initial-scale=1.0">
+        <title>CONTACT FORM</title>
+        <link rel="stylesheet" href="style.css">
+        <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
+    </head>
+    <body>
+        <div class="wrapper">
+            <header>Send us a Message</header>
+            <form action="#">
+                <div class="dbl-field">
+                    <div class="field">
+                        <input type="text" placeholder="Enter your name">
+                        <i class="fas fa-user"></i>
+                    </div>
+                    <div class="field">
+                        <input type="text" placeholder="Enter your email">
+                        <i class="fas fa-envelope"></i>
+                    </div>
+                </div>
+                <div class="dbl-field">
+                    <div class="field">
+                        <input type="text" placeholder="Enter your phone">
+                        <i class="fas fa-phone-alt"></i>
+                    </div>
+                    <div class="field">
+                        <input type="text" placeholder="Enter your website">
+                        <i class="fas fa-globe"></i>
+                    </div>
+                </div>
+                <div class="message">
+                    <textarea placeholder="write your message"></textarea>
+                    <i class="material-icons">message</i>
+                </div>
+                <div class="button-area">
+                    <button type="submit">Send Message</button>
+                    <span>Sending your message...</span>
+                </div>
+            </form>
+        </div>
+
+    </body>
+</html> 
