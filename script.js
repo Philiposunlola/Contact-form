272675dac91545b33123fad8be1be0c7f425c1a7
Philip <philiposun@gmail.com>
2023-09-30 00:33:06 +0100

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ statusTxt = form.querySelector(".button-area span");
 
 form.onsubmit = (e) => {
     e.preventDefault(); //preventing form from submitting
-    statusTxt.style.color = "red";
+    statusTxt.style.color = "rgb(57, 226, 114)";
     statusTxt.style.display = "block";
 
     let xhr = new XMLHttpRequest(); // creating new xml object
@@ -13,9 +13,14 @@ form.onsubmit = (e) => {
     xhr.onload = ()=> { //once ajax loaded
         if (xhr.readyState == 4 && xhr.status == 200) { //if ajax response status is 200 & ready status is 4 means there is no error
             let response = xhr.response; //storing ajax response in a response varaible
-            //if response is an error like enter valid email address hen we'll change status color to red 
+            //if response is an error like enter valid email address hen we'll change status color to red  else reset the form
            if (response.indexOf("Email and password field are required!") != -1 || response.indexOf("Enter a valid email address!") || response.indexOf("Sorry, failed to send your message!")) {
                 statusTxt.style.color = "red";
+           }else{
+                form.reset();
+                setTimeout(() => {
+                    statusTxt.style.display = "none";
+                }, 3000); //hide the statusTxt under 3 seconds if the message is sent
            }
             statusTxt.innerText = response;
         }
