d5ab0dbc2cefdd1c8988c68023e1ba22783e5261
Philip <philiposun@gmail.com>
2023-09-29 01:54:49 +0100

    Update script.js

--- script.js
+++ script.js
@@ -6,4 +6,12 @@ statusTxt = form.querySelector(".button-area span");
 form.onsubmit = (e) => {
     e.preventDefault(); //preventing form from submitting
     statusTxt.style.display = "block";
+
+    let xhr = new XMLHttpRequest(); // creating new xml object
+    xhr.open("POST", "message.php", true); //sending post request to message.php file
+    xhr.onload = ()=> { //once ajax loaded
+        if (xhr.readyState == 4 && xhr.status == 200) {
+            
+        }
+    }
 }
