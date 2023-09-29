c330cbfe146f97d608e2b001878807842d61d898
Philip <philiposun@gmail.com>
2023-09-30 00:56:54 +0100

    Update message.php

--- message.php
+++ message.php
@@ -7,6 +7,12 @@
     $message = $_POST['message'];
 
     if(!empty($email) && !empty($message)){// of email and message field is not empty
+        if (filter_var($email, FILTER_VALIDATE_EMAIL)) { // if user entered email is valid
+            $receiver = "philiposun@gmail.com"; // email receiver email address
+            $subject = "From: $name <$email>";
+            $body = "Name: $name\nEmail: $email\nPhone: $phone\nWebsite: $website\n\nMessage: $message\n\nRegards,\n$name";
+            $sender = "Fromm: $email"; //sender email
+        }
 
     }else {
         echo "Email and password field is required";
