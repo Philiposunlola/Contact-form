b746937083ee6d9595c10ca801fbacbe9220cdec
Philip <philiposun@gmail.com>
2023-09-30 01:08:03 +0100

    Update message.php

--- message.php
+++ message.php
@@ -9,11 +9,18 @@
     if(!empty($email) && !empty($message)){// of email and message field is not empty
         if (filter_var($email, FILTER_VALIDATE_EMAIL)) { // if user entered email is valid
             $receiver = "philiposun@gmail.com"; // email receiver email address
-            $subject = "From: $name <$email>";
+            $subject = "From: $name <$email>"; //subject of the email. Subjec looks like From: PhilipDcoder <abc@gmail.com>
+            //merging concating all user values inside body varaible. \n is used for new line
             $body = "Name: $name\nEmail: $email\nPhone: $phone\nWebsite: $website\n\nMessage: $message\n\nRegards,\n$name";
             $sender = "Fromm: $email"; //sender email
+            if (mail($receiver, $subject, $body, $sender)) { //mail() is a inbuilt php function to send mail
+                echo "Your message has been sent";
+            }else {
+               echo  "Sorry, failed to send your message!";
+            }
+        }else {
+            echo "Enter a valid email address";
         }
-
     }else {
         echo "Email and password field is required";
     }
