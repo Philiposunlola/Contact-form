0b2fc10e5cb2efb5d76f4ebcc0690d2f7c8f3d70
Philip <philiposun@gmail.com>
2023-10-03 03:02:24 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,21 @@
 //Contact Form in PHP
-const form = document.querySelector("form"),
+const form = document.querySelector("form");
 statusTxt = form.querySelector(".button-area span");
+
 form.onsubmit = (e)=>{
   e.preventDefault();
-  statusTxt.style.color = "#4fed86";
+  statusTxt.style.color = "#69e36f";
   statusTxt.style.display = "block";
-  statusTxt.innerText = "Sending your message...";
-  form.classList.add("disabled");
+
+//   statusTxt.innerText = "Sending your message...";
+//   form.classList.add("disabled");
 
   let xhr = new XMLHttpRequest();
   xhr.open("POST", "message.php", true);
   xhr.onload = ()=>{
     if(xhr.readyState == 4 && xhr.status == 200){
       let response = xhr.response;
-      if(response.indexOf("required") != -1 || response.indexOf("valid") != -1 || response.indexOf("failed") != -1){
+      if(response.indexOf("Email and message field is required!") != -1 || response.indexOf("Enter a valid email address!") != -1 || response.indexOf("Sorry, failed to send your message!") != -1){
         statusTxt.style.color = "red";
       }else{
         form.reset();
