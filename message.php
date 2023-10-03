fec740b61c2617ea8d2b3e26a57d60305fd48b76
Philip <philiposun@gmail.com>
2023-10-03 02:43:10 +0100

    Update message.php

--- message.php
+++ message.php
@@ -12,10 +12,10 @@ ini_set('smtp_port', 587);
 
   if(!empty($email) && !empty($message)){
     if(filter_var($email, FILTER_VALIDATE_EMAIL)){
-      $receiver = "iamphiliposun@gmail.com"; //enter that email address where you want to receive all messages
+      $receiver = "receiver_email_address"; //enter that email address where you want to receive all messages
       $subject = "From: $name <$email>";
       $body = "Name: $name\nEmail: $email\nPhone: $phone\nWebsite: $website\n\nMessage:\n$message\n\nRegards,\n$name";
-      $sender = "From: philiposun@gmail.com";
+      $sender = "From: $email";
       if(mail($receiver, $subject, $body, $sender)){
          echo "Your message has been sent";
       }else{
