be467c960494be48f9737716c8163155ba918da5
Philip <philiposun@gmail.com>
2023-10-05 00:07:40 +0100

    Update message.php

--- message.php
+++ message.php
@@ -9,7 +9,7 @@
   if(!empty($email) && !empty($message)){
     if(filter_var($email, FILTER_VALIDATE_EMAIL)){
       $receiver = "receiver_email_address"; //enter that email address where you want to receive all messages
-      $subject = "From: $name <$email>";
+      $subject = "From: philiposun@gmail.com";
       $body = "Name: $name\nEmail: $email\nPhone: $phone\nWebsite: $website\n\nMessage:\n$message\n\nRegards,\n$name";
       $sender = "From: $email";
       if(mail($receiver, $subject, $body, $sender)){
