14b42972c463412c1dc8faf780be635ec1339033
Philip <philiposun@gmail.com>
2023-10-05 01:48:47 +0100

    Update message.php

--- message.php
+++ message.php
@@ -8,7 +8,7 @@
 
   if(!empty($email) && !empty($message)){
     if(filter_var($email, FILTER_VALIDATE_EMAIL)){
-      $receiver = "iamphiliposun@gmail.ccom"; //enter that email address where you want to receive all messages
+      $receiver = "philiposun@gmail.com"; //enter that email address where you want to receive all messages
       $subject = "From: $name <$email>";
       $body = "Name: $name\nEmail: $email\nPhone: $phone\nWebsite: $website\n\nMessage:\n$message\n\nRegards,\n$name";
       $sender = "From: philiposun@gmail.com";
