4369b10006936843cf351bdc3efceb3c80b7d674
Philip <philiposun@gmail.com>
2023-10-05 01:22:38 +0100

    Update message.php

--- message.php
+++ message.php
@@ -12,7 +12,7 @@
       $subject = "From: $name <$email>";
       $body = "Name: $name\nEmail: $email\nPhone: $phone\nWebsite: $website\n\nMessage:\n$message\n\nRegards,\n$name";
       $sender = "From: philiposun@gmail.com";
-      if(mail($receiver, $subject, $body, $sender)){
+      if(mail($receiver, $subject, $body)){
          echo "Your message has been sent";
       }else{
          echo "Sorry, failed to send your message!";
