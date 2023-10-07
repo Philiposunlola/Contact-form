d6d7c18ceba0349366ac6b8f8d5ff3b6af18dbb1
Philip <philiposun@gmail.com>
2023-10-07 01:59:25 +0100

    Update mail.php

--- mail.php
+++ mail.php
@@ -1,12 +1,12 @@
 <?php
-    // $receiver = "iamphiliposun@gmail.com";
-    // $subject = "Email text via PHP using localhost";
-    // $body = "Hi, there...this is a text email sent from localhost";
-    // $sender = "From: philiposun@gmail.com";
+    $receiver = "philiposun@gmail.com";
+    $subject = "Email text via PHP using localhost";
+    $body = "Hi, there...this is a text email sent from localhost";
+    $sender = "From: philiposun@gmail.com";
 
-    // if (mail($receiver, $subject, $body, $sender)) {
-    //     echo "Email sent successfully to $receiver";
-    // } else {
-    //     echo "Sorry, failed while sending mail";
-    // }
+    if (mail($receiver, $subject, $body, $sender)) {
+        echo "Email sent successfully to $receiver";
+    } else {
+        echo "Sorry, failed while sending mail";
+    }
 ?>
