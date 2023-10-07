c93209c6cfc817bc49e130d92397d98215f47b35
Philip <philiposun@gmail.com>
2023-10-07 01:18:13 +0100

    Update message.php

--- message.php
+++ message.php
@@ -24,13 +24,13 @@ try {
     $mail->isSMTP();                                            //Send using SMTP
     $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
     $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-    $mail->Username   = 'youremail';                     //SMTP username
+    $mail->Username   = 'philiposun@gmail.com';                     //SMTP username
     $mail->Password   = 'secret';                               //SMTP password
     $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
     $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
 
     //Recipients
-    $mail->setFrom('email', 'Your Name');
+    $mail->setFrom('philiposun@gmail.com', 'Philip');
     $mail->addAddress($email);             
    
     //Content
